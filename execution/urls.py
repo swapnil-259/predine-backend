cd6db8f7b227b9177a4a85a58dfad2155ed557ab
swapnil-259 <swapnilagrawal259@gmail.com>
2024-06-20 20:05:48 +0530

    fix: owner data api

--- execution/urls.py
+++ execution/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('owner_registration/', views.owner_registration),
     path('get_role/', views.get_role_type),
     path('get_res/', views.get_res_type),
+    path('owner_list/', views.owner_list),
     path('view_owners/', views.view_owners)
-
 ]
